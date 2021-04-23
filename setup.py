2cf96e7a6208aa5d70006062bde0e199ef1dec14
YeonwooSung <neos960518@gmail.com>
2021-04-23 21:33:40 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup, find_packages
+import os.path as path
+
+basedir = path.abspath(path.dirname(__file__))
+with open(path.join(basedir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name='pyglom',
@@ -9,6 +15,8 @@ setup(
     author='Yeonwoo Sung',
     author_email='neos960518@gmail.com',
     url='https://github.com/YeonwooSung/GLOM',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     keywords=[
         'AI',
         'artificial intelligence',
@@ -23,10 +31,10 @@ setup(
         'torch>=1.6'
     ],
     classifiers=[
-        'Development Status :: 1 - Beta',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.6'
     ],
 )
